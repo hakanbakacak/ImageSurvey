c4d433a89d3878c503e84189799e9c9c16c04e53
hakanbakacak <hakanbakacak98@gmail.com>
2020-05-19 20:18:34 +0300

    add timer to survey page

--- lib/questionPage.dart
+++ lib/questionPage.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'homePage.dart';
-
 
 class QuestionPage extends StatefulWidget {
   static const String route = '/survey';
@@ -13,17 +14,91 @@ class _QuestionPageState extends State<QuestionPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Image.asset('assets/logo.png', fit: BoxFit.contain, height: 50,),
+        title: Image.asset(
+          'assets/logo.png',
+          fit: BoxFit.contain,
+          height: 50,
+        ),
         actions: [
-          FlatButton(onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => HomePage()));
-          }, child: Text("About Us",), ),
-          FlatButton(onPressed: (){
-             Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => QuestionPage()));
-          }, child: Text("Survey",), )
+          FlatButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => HomePage()));
+            },
+            child: Text(
+              "About Us",
+            ),
+          ),
+          FlatButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (BuildContext context) => QuestionPage()));
+            },
+            child: Text(
+              "Survey",
+            ),
+          )
         ],
       ),
-      body: Container(),
+      body: ImageTimer(),
     );
+  }
+}
+
+class ImageTimer extends StatefulWidget {
+  @override
+  _ImageTimerState createState() => _ImageTimerState();
+}
+
+class _ImageTimerState extends State<ImageTimer> {
+  bool question = true;
+  @override
+  void initState() {
+    Timer timer = new Timer(new Duration(seconds: 5), () {
+      setState(() {
+        if (question == true) {
+          question = false;
+        } else {
+          question = true;
+        }
+      });
+      debugPrint("Print after 5 seconds");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (question == true) {
+      return Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/logo.png",
+                fit: BoxFit.cover,
+                height: 500,
+              ),
+            ],
+          ),
+        ),
+      );
+    } else {
+      return Center(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(icon: Icon(Icons.add), onPressed:(){}),
+            IconButton(icon: Icon(Icons.ac_unit), onPressed: (){})
+          ],
+        ),
+      );
+    }
   }
 }
